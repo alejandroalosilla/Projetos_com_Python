f3b1896e3ba27bf0df4cc84ea563acd919273b1f
Alejandro Alosilla <alejandroalosilla@gmail.com>
2023-06-04 18:48:40 -0300

    up 1

---
+++ Analisar_Planilhas_de_Vendas_de_Produtos/analisador_de_planilhas_de_vendas.py
@@ -0,0 +1,21 @@
+# Lógica de programação
+
+# Passo 0 - Entender o desafio que você quer resolver
+
+# Passo 1 - Percorrer todos os arquivos da pasta base de dados (Pasta Vendas)
+import os
+
+lista_arquivo = os.listdir("/content/drive/MyDrive/Curso Básico de Python/Vendas")
+
+# Passo 2 - Importar as bases de dados de vendas
+for arquivo in lista_arquivo:
+  # Importar o arquivo
+  print(f"/content/drive/MyDrive/Curso Básico de Python/Vendas/{arquivo}")
+
+# Passo 3 - Tratar / Compilar as bases de dados
+
+# Passo 4 - Calcular o produto mais vendido (em quantidade)
+
+# Passo 5 - Calcular o produto que mais faturou (em faturamento)
+
+# Passo 6 - Calcular a loja / cidade que mais vendeu (em faturamento)
